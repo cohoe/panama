88bfd13aa1e5fd2b94a0fb32b1b1ec138be8d2de
Grant Cohoe <grant@grantcohoe.com>
2021-03-04 19:39:14 -0500

    add sample thing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({name='Foo'}) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Hello {name}
         </p>
         <a
           className="App-link"

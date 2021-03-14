e8c3603e8e898370d1059501fb9bfa99d9260e0f
Grant Cohoe <grant@grantcohoe.com>
2021-03-13 23:13:49 -0500

    more broken shit

--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -1,6 +1,9 @@
 import React, {useState, useEffect} from 'react'
-import {Card, CardContent, Container} from '@material-ui/core'
+import {Card, CardContent, Container, ListItem, Tabs, Tab} from '@material-ui/core'
+import {TabPanel} from '@material-ui/lab'
+// import TabPanel from '@material-ui/core/TabsP'
 import {Spec} from '../Spec'
+import {Text} from "../Text";
 
 // https://stackoverflow.com/questions/56670140/react-string-prop-passing-down-as-an-object
 export const Cocktail = ({cocktail_slug, spec_slug}) => {
@@ -20,13 +23,13 @@ export const Cocktail = ({cocktail_slug, spec_slug}) => {
 
     // https://learus.github.io/react-material-ui-carousel/
     // @TODO that
-    const lookupSpec = (spec_slug) => {
-        for (let i=0; i<cocktail.specs.length; i++) {
+    const lookupSpecIndex = (spec_slug) => {
+        for (let i = 0; i < cocktail.specs.length; i++) {
             if (cocktail.specs[i].slug === spec_slug) {
-                return cocktail.specs[i]
+                return i
             }
         }
-        return cocktail.specs[0]
+        return 0
     }
 
     return (
@@ -36,7 +39,17 @@ export const Cocktail = ({cocktail_slug, spec_slug}) => {
                 <Card key={cocktail.slug}>
                     <CardContent>
                         <p>{cocktail.display_name}</p>
-                        <Spec cocktail_slug={cocktail.slug} spec={lookupSpec(spec_slug)}/>
+                        <Tabs value={spec_slug || cocktail.specs[0].slug}>
+                            {cocktail.specs.map((spec, i) => (
+                                <Tab value={spec.slug} index={i} label={spec.display_name}/>
+                            ))}
+                        </Tabs>
+                        <TabPanel value={"foo"}>asd</TabPanel>
+                        {/*{cocktail.specs.map((spec, i) => (*/}
+                        {/*    <TabPanel value={spec.slug} index={i}>*/}
+                        {/*        <Spec cocktail_slug={cocktail_slug} spec={spec}/>*/}
+                        {/*    </TabPanel>*/}
+                        {/*))}*/}
                     </CardContent>
                 </Card>
             )}

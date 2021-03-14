326bdfc0c9e2f5ade38caae2c40141e09998ccb5
Grant Cohoe <grant@grantcohoe.com>
2021-03-13 22:15:53 -0500

    carousel

--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {Card, CardContent, Container} from '@material-ui/core'
 import {Spec} from '../Spec'
+import Carousel from 'react-material-ui-carousel'
 
 // https://stackoverflow.com/questions/56670140/react-string-prop-passing-down-as-an-object
 export const Cocktail = ({cocktail_slug, spec_slug}) => {
@@ -18,15 +19,13 @@ export const Cocktail = ({cocktail_slug, spec_slug}) => {
         fetchData(cocktail_slug)
     }, [cocktail_slug])
 
-    // https://learus.github.io/react-material-ui-carousel/
-    // @TODO that
-    const lookupSpec = (spec_slug) => {
-        for (let i=0; i<cocktail.specs.length; i++) {
+    const lookupSpecIndex = (spec_slug) => {
+        for (let i = 0; i < cocktail.specs.length; i++) {
             if (cocktail.specs[i].slug === spec_slug) {
-                return cocktail.specs[i]
+                return i
             }
         }
-        return cocktail.specs[0]
+        return 0
     }
 
     return (
@@ -36,7 +35,15 @@ export const Cocktail = ({cocktail_slug, spec_slug}) => {
                 <Card key={cocktail.slug}>
                     <CardContent>
                         <p>{cocktail.display_name}</p>
-                        <Spec cocktail_slug={cocktail.slug} spec={lookupSpec(spec_slug)}/>
+                        <Carousel autoPlay={false} cycleNavigation={false} timeout={0} index={lookupSpecIndex(spec_slug)}
+                                  navButtonsAlwaysVisible={true} fullHeightHover={true} navButtonsWrapperProps={{ style: { height: 'unset'}}}>
+                            {cocktail.specs.map((spec, i) => (
+                                <Container>
+                                    <Spec cocktail_slug={cocktail.slug} spec={spec}/>
+                                </Container>
+                            ))}
+                        </Carousel>
+                        {/*<Spec cocktail_slug={cocktail.slug} spec={lookupSpec(spec_slug)}/>*/}
                     </CardContent>
                 </Card>
             )}

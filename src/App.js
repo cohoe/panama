2ccb2957db4852e404c390158d3bba27339a7794
Grant Cohoe <grant@grantcohoe.com>
2021-03-28 18:39:40 -0400

    At least something finally works

--- src/App.js
+++ src/App.js
@@ -9,6 +9,44 @@ import {Container} from "@material-ui/core"
 
 // const fruits = ['apple', 'banana', 'orange']
 
+
+
+// BEGIN API JUNK
+
+// https://github.com/swagger-api/swagger-codegen/issues/10451
+
+
+
+var JamaicaApi = require('jamaica_api');
+
+var api = new JamaicaApi.V1authApi()
+// api.basePath = 'http://localhost:8080/api'
+api.getAuthInfoEndpoint().then(function() {
+    console.log('API called successfully.');
+}, function(error) {
+    console.error(error);
+});
+
+const cocktailsApi = new JamaicaApi.V1cocktailsApi()
+cocktailsApi.getCocktailsEndpoint().then(function(data) {
+    console.log(data)
+})
+
+
+// console.log(api.V1listsApi())
+
+
+
+
+
+
+// END API JUNK
+
+
+
+
+
+
 function App({name = 'Foo'}) {
     return (
         <div className="App">

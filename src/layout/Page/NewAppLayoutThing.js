f964acdd1d6a94318c69055890c7396c66271d15
Grant Cohoe <grant@grantcohoe.com>
2021-04-02 23:12:49 -0400

    something that works

--- src/layout/Page/NewAppLayoutThing.js
+++ src/layout/Page/NewAppLayoutThing.js
@@ -19,6 +19,9 @@ import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
 import MailIcon from '@material-ui/icons/Mail';
 
+
+import {CocktailList} from '../../components/Cocktail/CocktailList'
+
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
@@ -79,8 +82,26 @@ const useStyles = makeStyles((theme) => ({
     },
     content: {
         flexGrow: 1,
-        padding: theme.spacing(3),
+        // padding: theme.spacing(3),
+        width: `calc(100% - ${drawerWidth}px)`,
+        overflow: 'hidden',
+        display: 'flex',
+        flexDirection: 'column',
     },
+    scrollBar: {
+        width: '340px',
+        overflowY: 'scroll',
+        // flex: 1,
+    },
+    contentRoot: {
+        display: 'flex',
+        // flexDirection: 'column',
+        overflow: 'hidden',
+        flex: 1
+    },
+    contentArea: {
+        flex: 2
+    }
 }));
 
 export default function MiniDrawer() {
@@ -161,29 +182,37 @@ export default function MiniDrawer() {
             </Drawer>
             <main className={classes.content}>
                 <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-                </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-                </Typography>
+                <div className={classes.contentRoot}>
+                    <div className={classes.scrollBar}>
+                        <CocktailList />
+                    </div>
+                    <div className={classes.contentArea}>
+                        <Typography paragraph>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                            donec massa sapien faucibus et molestie ac.
+                        </Typography>
+                    </div>
+                </div>
+
+                {/*<Typography paragraph>*/}
+                {/*    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla*/}
+                {/*    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac*/}
+                {/*    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat*/}
+                {/*    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed*/}
+                {/*    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In*/}
+                {/*    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et*/}
+                {/*    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin*/}
+                {/*    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas*/}
+                {/*    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.*/}
+                {/*</Typography>*/}
             </main>
         </div>
     );

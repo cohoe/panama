66d957948174c32202dde0157e678a9a53655809
Grant Cohoe <grant@grantcohoe.com>
2021-07-18 17:58:27 -0400

    shelving it for now

--- src/components/Form/FormInputSelect.js
+++ src/components/Form/FormInputSelect.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {InputLabel, Select, MenuItem, FormControl} from '@material-ui/core'
-import {makeStyles} from "@material-ui/core/styles";
+// import {makeStyles} from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}))
+// const useStyles = makeStyles((theme) => ({}))
 
 export const FormInputSelect = ({children, items = {}, onChange, initialValue = ''}) => {
-    const classes = useStyles();
+    // const classes = useStyles();
     const [value, setValue] = React.useState(initialValue);
 
     const handleChange = (event) => {
@@ -18,7 +18,7 @@ export const FormInputSelect = ({children, items = {}, onChange, initialValue = 
             <InputLabel id="label">Construction:</InputLabel>
             <Select labelId="label" id="select" value={value} onChange={handleChange}>
                 {Object.keys(items).map((k) => {
-                    return (<MenuItem value={k}>{items[k]}</MenuItem>)
+                    return (<MenuItem value={k} key={k}>{items[k]}</MenuItem>)
                 })}
             </Select>
         </FormControl>
--- src/components/SearchForm/CocktailSearchForm.js
+++ src/components/SearchForm/CocktailSearchForm.js
@@ -2,15 +2,15 @@ import React, {useState, useEffect} from 'react'
 import {makeStyles} from "@material-ui/core/styles";
 import {FormGroup} from "@material-ui/core";
 import {FormInputSelect} from "../Form";
-import {useLocation, useParams} from "react-router-dom";
+import {useLocation, useParams, useHistory} from "react-router-dom";
 
 const useStyles = makeStyles({
     root: {},
 });
 
-const useQuery = () => {
-    return new URLSearchParams(useLocation().search);
-}
+// const useQuery = () => {
+//     return new URLSearchParams(useLocation().search);
+// }
 
 
 export const CocktailSearchForm = ({}) => {
@@ -18,8 +18,9 @@ export const CocktailSearchForm = ({}) => {
     const query = useQuery();
     console.log("Up Here")
     console.log(query.get('construction'))
+    const history = useHistory()
 
-    const [construction, setConstruction] = useState(query.get('construction'))
+    const [construction, setConstruction] = useState(query.get('construction') || '')
     // const handleConstruction = (event) => {
     //     setState({construction: event.target.value})
     // }
@@ -30,8 +31,17 @@ export const CocktailSearchForm = ({}) => {
 
     const handleSubmit = (event) => {
         console.log("SENDING")
+        console.log(event)<
         console.log(construction)
+        // query.append('construction', construction)
+        // history.push({search: `?construction={construction}`})
     }
+
+    // https://medium.com/@kate.pavlova/how-to-set-or-get-query-parameters-in-react-router-7d6e2440ede8
+    // useEffect(() => {
+    //     query.append('construction', construction)
+    //     history.push({search: query.toString()})
+    // }, [query, history])
 
     console.log("Hi")
     console.log(construction)
@@ -39,7 +49,7 @@ export const CocktailSearchForm = ({}) => {
     // https://upmostly.com/tutorials/react-onchange-events-with-examples
     return (
         <div className={classes.root}>
-            <form method={"get"} action={"#"}>
+            <form method={"GET"}>
                 <FormInputSelect items={{'shake': 'Shake', 'stir': 'Stir', 'swizzle': 'Swizzle', 'build': 'Build'}}
                                  onChange={(e) => setConstruction(e.target.value)} initialValue={construction}/>
                 {/*onChange={handleConstruction}/>*/}

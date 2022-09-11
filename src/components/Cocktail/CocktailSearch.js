f7dcdf6e81039fa8ca62f2f310a6f2ec42233db4
Grant Cohoe <grant@grantcohoe.com>
2022-09-11 15:26:13 -0400

    computer crash

--- src/components/Cocktail/CocktailSearch.js
+++ src/components/Cocktail/CocktailSearch.js
@@ -1,32 +1,13 @@
 import React from 'react'
-// import Chip from '@mui/material/Chip';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
-// import Stack from '@mui/material/Stack';
 import {Box, Stack, useTheme} from '@mui/material'
 import {styled} from "@mui/material/styles";
 import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
-import InputBase from '@mui/material/InputBase';
-
-
-const top100Films = [
-    {title: 'The Shawshank Redemption', year: 1994},
-    {title: 'The Godfather', year: 1972},
-    {title: 'The Godfather: Part II', year: 1974},
-    {title: 'The Dark Knight', year: 2008},
-    {title: '12 Angry Men', year: 1957},
-    {title: "Schindler's List", year: 1993},
-];
-
-const constructions = [
-    {value: "stir", label: "Stir"},
-    {value: "shake", label: "Shake"},
-    {value: "build", label: "Build"},
-    {value: "swizzle", label: "Swizzle"},
-]
+import Button from '@mui/material/Button';
 
 const SearchPaneWrapper = styled(Box)(({theme}) => ({
     label: 'SearchPaneWrapper',
@@ -37,7 +18,7 @@ const SearchPaneWrapper = styled(Box)(({theme}) => ({
     padding: theme.spacing(1, 1),
 }))
 
-const AutocompleteFormControl = ({id, options=[], placeholder="", label=""}) => {
+const AutocompleteFormControl = ({id, options = [], placeholder = "", label = "", onChange = null}) => {
     const theme = useTheme();
     return (
         <FormControl>
@@ -46,7 +27,7 @@ const AutocompleteFormControl = ({id, options=[], placeholder="", label=""}) => 
                 multiple
                 id={id}
                 options={options}
-                getOptionLabel={(option) => option.title}
+                getOptionLabel={(option) => option.display_name}
                 filterSelectedOptions
                 size={"small"}
                 sx={{marginTop: theme.spacing(2)}}
@@ -56,6 +37,7 @@ const AutocompleteFormControl = ({id, options=[], placeholder="", label=""}) => 
                         variant="standard"
                         // label="Include Components"
                         placeholder={placeholder}
+                        onChange={onChange}
                     />
                 )}
             />
@@ -63,40 +45,32 @@ const AutocompleteFormControl = ({id, options=[], placeholder="", label=""}) => 
     )
 }
 
-const ToggleButtonGroupControl = ({id, buttons=[], label=""}) => {
+const ToggleButtonGroupControl = ({id, buttons = [], label = "", onChange, queryParam}) => {
     const theme = useTheme();
     const [options, setOptions] = React.useState([])
 
     const updateOptions = (event, newOptions) => {
+        console.log("Setting toggle buttons to: ")
+        console.log(newOptions)
         setOptions(newOptions)
+        onChange(event)
     }
     return (
         <FormControl>
             <InputLabel shrink variant="standard">{label}</InputLabel>
             <ToggleButtonGroup
                 id={id}
+                name={id}
                 value={options}
                 onChange={updateOptions}
                 aria-label={id}
                 sx={{marginTop: theme.spacing(2)}}
             >
                 {buttons.map((option, i) => (
-                    <ToggleButton key={option.value} value={option.value} aria-label={option.value}>
-                        {option.label}
+                    <ToggleButton key={option.slug} value={option.slug} aria-label={option.slug}>
+                        {option.display_name}
                     </ToggleButton>
                 ))}
-                {/*<ToggleButton value="stir" aria-label="stir">*/}
-                {/*    Stir*/}
-                {/*</ToggleButton>*/}
-                {/*<ToggleButton value="shake" aria-label="shake">*/}
-                {/*    Shake*/}
-                {/*</ToggleButton>*/}
-                {/*<ToggleButton value="build" aria-label="build">*/}
-                {/*    Build*/}
-                {/*</ToggleButton>*/}
-                {/*<ToggleButton value="swizzle" aria-label="swizzle">*/}
-                {/*    Swizzle*/}
-                {/*</ToggleButton>*/}
             </ToggleButtonGroup>
         </FormControl>
     )
@@ -105,26 +79,66 @@ const ToggleButtonGroupControl = ({id, buttons=[], label=""}) => {
 export const CocktailSearch = () => {
     const theme = useTheme();
     const [components, setComponents] = React.useState([])
+    const [constructions, setConstructions] = React.useState([])
     const [loading, setLoading] = React.useState(true)
+    const [formValues, setFormValues] = React.useState({});
 
-    const fetchIngredients = async () => {
-        const response = await fetch(`http://localhost:8080/api/v1/ingredients`)
-        const json = await response.json()
+    const fetchSearchData = async () => {
+        let response = await fetch(`http://localhost:8080/api/v1/ingredients`)
+        let json = await response.json()
         setComponents(json)
+
+        response = await fetch(`http://localhost:8080/api/v1/constructions`)
+        json = await response.json()
+        setConstructions(json)
+
         setLoading(false)
     }
 
     React.useEffect(() => {
-        fetchIngredients()
+        fetchSearchData()
     }, [])
 
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(formValues);
+    }
+    // https://onestepcode.com/creating-a-material-ui-form/
+    const handleInputChange = (e) => {
+        console.log("Calling handleInputChange")
+        console.log(e)
+        const {name, value} = e.target;
+        setFormValues({
+            ...formValues,
+            [name]: value,
+        });
+        console.log("Form values are now:")
+        console.log(formValues)
+    };
+
     return (
-        <SearchPaneWrapper theme={theme}>
-            <Stack spacing={2} sx={{width: 'inherit', overflow: 'hidden'}}>
-                <AutocompleteFormControl id={"include-components"} placeholder={"Include..."} label={"Include Components"} options={top100Films} />
-                <AutocompleteFormControl id={"exclude-components"} placeholder={"Exclude..."} label={"Exclude Components"} options={top100Films} />
-                <ToggleButtonGroupControl id={"construction"} label={"Construction"} buttons={constructions} />
-            </Stack>
-        </SearchPaneWrapper>
+        <>
+            {loading && <p>Loading...</p>}
+            {!loading && (
+                <SearchPaneWrapper theme={theme}>
+                    <form onSubmit={handleSubmit}>
+                        <Stack spacing={2} sx={{width: 'inherit', overflow: 'hidden'}}>
+                            <AutocompleteFormControl id={"include-components"} placeholder={"Include..."}
+                                                     label={"Include Components"} options={components}
+                                                     onChange={handleInputChange}/>
+                            <AutocompleteFormControl id={"exclude-components"} placeholder={"Exclude..."}
+                                                     label={"Exclude Components"} options={components}
+                                                     onChange={handleInputChange}/>
+                            <ToggleButtonGroupControl id={"construction"} label={"Construction"}
+                                                      buttons={constructions}
+                                                      onChange={handleInputChange} queryParam={"construction"}/>
+                            <Button variant="contained" color="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Stack>
+                    </form>
+                </SearchPaneWrapper>
+            )}
+        </>
     )
 }

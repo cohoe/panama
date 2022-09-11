3c1fbb4d6fddf0655f2062fca59bc6c3436d0774
Grant Cohoe <grant@grantcohoe.com>
2022-09-11 14:06:26 -0400

    enough of a form to get to work

--- src/components/Cocktail/CocktailSearch.js
+++ src/components/Cocktail/CocktailSearch.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import Chip from '@mui/material/Chip';
+// import Chip from '@mui/material/Chip';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 // import Stack from '@mui/material/Stack';
 import {Box, Stack, useTheme} from '@mui/material'
 import {styled} from "@mui/material/styles";
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import InputBase from '@mui/material/InputBase';
+
 
 const top100Films = [
     {title: 'The Shawshank Redemption', year: 1994},
@@ -19,46 +25,88 @@ const SearchPaneWrapper = styled(Box)(({theme}) => ({
     label: 'SearchPaneWrapper',
     width: '100%',
     minHeight: '50px',
-    backgroundColor: 'red',
+    backgroundColor: theme.palette.background.default,
     display: 'flex',
     padding: theme.spacing(1, 1),
 }))
 
 export const CocktailSearch = () => {
     const theme = useTheme();
+    const [constructions, setConstructions] = React.useState([])
+
+    const updateConstructions = (event, newConstructions) => {
+        setConstructions(newConstructions)
+    }
+
     return (
         <SearchPaneWrapper theme={theme}>
             <Stack spacing={2} sx={{width: 'inherit', overflow: 'hidden'}}>
-                <Autocomplete
-                    multiple
-                    id="include-components"
-                    options={top100Films}
-                    getOptionLabel={(option) => option.title}
-                    size={"small"}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            variant="standard"
-                            label="Include Components"
-                            placeholder="Include..."
-                        />
-                    )}
-                />
-                <Autocomplete
-                    multiple
-                    id="exclude-components"
-                    options={top100Films}
-                    getOptionLabel={(option) => option.title}
-                    size={"small"}
-                    renderInput={(params) => (
-                        <TextField
-                            {...params}
-                            variant="standard"
-                            label="Exclude Components"
-                            placeholder="Exclude..."
-                        />
-                    )}
-                />
+                <FormControl>
+                    <InputLabel shrink variant="standard" forHtml={"include-components"}>Include Components</InputLabel>
+                    <Autocomplete
+                        multiple
+                        id="include-components"
+                        options={top100Films}
+                        getOptionLabel={(option) => option.title}
+                        filterSelectedOptions
+                        size={"small"}
+                        sx={{marginTop: theme.spacing(2)}}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                variant="standard"
+                                // label="Include Components"
+                                placeholder="Include..."
+                            />
+                        )}
+                    />
+                </FormControl>
+                <FormControl>
+                    <InputLabel shrink variant="standard" forHtml={"exclude-components"}>Exclude Components</InputLabel>
+                    <Autocomplete
+                        multiple
+                        id="exclude-components"
+                        options={top100Films}
+                        getOptionLabel={(option) => option.title}
+                        filterSelectedOptions
+                        size={"small"}
+                        sx={{marginTop: theme.spacing(2)}}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                variant="standard"
+                                // label="Exclude Components"
+                                placeholder="Exclude..."
+                            />
+                        )}
+                    />
+                </FormControl>
+                {/*<FormControlLabel label={"Construction"} />*/}
+                <FormControl>
+                    <InputLabel shrink variant="standard" forHtml={"construction-input"}>Construction</InputLabel>
+                    {/*<InputBase>*/}
+                    <ToggleButtonGroup
+                        id={"construction-input"}
+                        value={constructions}
+                        onChange={updateConstructions}
+                        aria-label="construction"
+                        sx={{marginTop: theme.spacing(2), justifyContent: 'center'}}
+                    >
+                        <ToggleButton value="stir" aria-label="stir">
+                            Stir
+                        </ToggleButton>
+                        <ToggleButton value="shake" aria-label="shake">
+                            Shake
+                        </ToggleButton>
+                        <ToggleButton value="build" aria-label="build">
+                            Build
+                        </ToggleButton>
+                        <ToggleButton value="swizzle" aria-label="swizzle">
+                            Swizzle
+                        </ToggleButton>
+                    </ToggleButtonGroup>
+                    {/*</InputBase>*/}
+                </FormControl>
             </Stack>
         </SearchPaneWrapper>
     )
